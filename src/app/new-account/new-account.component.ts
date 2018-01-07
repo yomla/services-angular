a816ce15864434f5f1ea211976bb891a2af46d98
Mladen <djoml8bre@gmail.com>
2018-01-07 14:44:15 +0100

    Commit-7 Using Services to Comunicate between Components, subscribe() method

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,7 +12,11 @@ import { AccountsService } from '../accounts.service';
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
-              private accountsService: AccountsService) {}
+              private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
    this.accountsService.addAccount(accountName, accountStatus);
